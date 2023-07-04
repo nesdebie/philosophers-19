f7d6b3cfb873ddb1d3aa8ac96fbb4f82ca210034
Nestor Debiesme <nesdebie@fu-r7-p5.s19.be>
2023-07-04 09:19:44 +0200

    ajout folder bonus (to do)

---
+++ philo_bonus/src/main.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nesdebie <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/04 14:16:47 by nesdebie          #+#    #+#             */
+/*   Updated: 2023/06/06 12:44:24 by nesdebie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/philo.h"
+
+int	main(int ac, char **av)
+{
+	t_rules	rules;
+	int		ret;
+
+	if (ac < 5 || ac > 6)
+		return (ft_error("Wrong amount of arguments (min. 5 to 6)"));
+	ret = init_manager(&rules, av);
+	if (ret == WRONG_ARGS)
+		return (ft_error("At least one wrong argument\nPlease try again !"));
+	else if (ret == MUTEX_FAIL)
+		return (ft_error("pthread_mutex_init() failed"));
+	else if (ret == MALLOC_FAIL)
+		return (ft_error("malloc() failed"));
+	if (!ft_create_threads(&rules))
+	{
+		free (rules.forks);
+		free (rules.phi);
+		return (ft_error("pthread_create() failed"));
+	}
+	return (0);
+}
